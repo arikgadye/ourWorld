031b85926bdfff2b5a9a4c53a5cd3cc5f458e496
Arik Gadye <arikdovgadye@gmail.com>
2014-07-27 16:48:08 -0700

    Added google maps api to homepage, event listener on click to grab lat/long coordinates

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.1.2'
+ruby '2.0.0'
 
 # PostgreSQL driver
 gem 'pg'
@@ -20,7 +20,7 @@ gem 'activerecord'
 
 gem 'rake'
 
-gem 'shotgun'		
+gem 'shotgun'
 
 group :test do
   gem 'shoulda-matchers'
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 	
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(33.189708, -14.765625),
+      zoom: 3
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
 
+    google.maps.event.addListener(map, 'click', function(e) {
+    var latitude = e.latLng.lat();
+    var longitude = e.latLng.lng();
+    console.log(latitude + ', ' + longitude);
+  });
+  }
+
+    google.maps.event.addDomListener(window, 'load', initialize); 
 
 });
